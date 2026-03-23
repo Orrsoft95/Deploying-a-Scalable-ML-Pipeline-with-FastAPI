e3e6da11da3f6b3b6ce1c4d29eab46aa9d2b7e1d
Marqez Orr <morr29@wgu.edu>
2026-03-23 19:41:39 -0400

    Added code to complete performance_on_categorical_slice in ml>model.py

--- ml/model.py
+++ ml/model.py
@@ -125,12 +125,18 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    # DONE: implement the function
     X_slice, y_slice, _, _ = process_data(
-        # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
+        X= data[data[column_name] == slice_value],
+        categorical_features= categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
+
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model=model, X=X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
