e66416f3d02b808e383943f56e66ce37d0214b55
Marqez Orr <morr29@wgu.edu>
2026-03-24 09:36:48 -0400

    Added code for test_model_classification_metrics test in test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,13 +1,39 @@
 import pytest
 # TODO: add necessary import
+import pandas as pd
+from sklearn.metrics import precision_score, recall_score, f1_score
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+PRECISION_THRESHOLD = 0.75
+RECALL_THRESHOLD = 0.55
+F1_THRESHOLD = 0.60
+
+
+# DONE: implement the first test. Change the function name and input as needed
+def test_model_classification_metrics(y, preds):
     """
-    # add description for the first test
+    # Test to confirm that the model's classification metrics are AT LEAST 0.75 precision, 0.55 recall, 0.60 F1-score when run against the test dataset.
+    Inputs
+    ------
+    y: np.array
+        Known labels, binarized
+    preds: np.array
+        Predicted labels, binarized
     """
-    # Your code here
-    pass
+    precision = precision_score(y, preds)
+    recall = recall_score(y, preds)
+    f1 = f1_score(y, preds)
+
+    assert precision >= PRECISION_THRESHOLD, (
+        f"Calculated precision of {precision} is below required threshold of {PRECISION_THRESHOLD}"
+    )
+
+    assert recall >= RECALL_THRESHOLD, (
+        f"Calculated recall of {recall} is below required threshold of {RECALL_THRESHOLD}"
+    )
+
+    assert f1 >= F1_THRESHOLD, (
+        f"Calculated F1-score of {f1} is below required threshold of {F1_THRESHOLD}"
+    )
 
 
 # TODO: implement the second test. Change the function name and input as needed
